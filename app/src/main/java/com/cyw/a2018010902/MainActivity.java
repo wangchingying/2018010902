7a76327b6903d0b9f8f12ff7075c497e72164251
totorowg <wangchingying@gmail.com>
2018-01-09 16:52:33 +0800

    error

--- app/src/main/java/com/cyw/a2018010902/MainActivity.java
+++ app/src/main/java/com/cyw/a2018010902/MainActivity.java
@@ -1,8 +1,12 @@
 package com.cyw.a2018010902;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
+        if(Build.VERSION.SDK_INT>=26)
+        {
+            regChannel();
+        }
+
+    }
+    @TargetApi(Build.VERSION_CODES.O)
+    public void regChannel() {
         channelLove = new NotificationChannel(
                 idLove,
                 "Channel Love",
@@ -23,18 +36,37 @@ public class MainActivity extends AppCompatActivity {
         channelLove.setDescription("最重要的人");
         channelLove.enableLights(true);
         channelLove.enableVibration(true);
-
-        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         nm.createNotificationChannel(channelLove);
     }
-
+    @TargetApi(Build.VERSION_CODES.O)
+    @SuppressWarnings("deprecation")
     public void click1(View v)
     {
 
-        Notification.Builder builder = new Notification.Builder(MainActivity.this, idLove);
+        Notification.Builder builder;
+        if (Build.VERSION.SDK_INT >= 26)
+        {
+            builder = new Notification.Builder(MainActivity.this, idLove);
+        }
+        else
+        {
+            builder = new Notification.Builder(MainActivity.this);
+        }
+        //這裡設定點notification就跳到下一頁
+        Intent it = new Intent(MainActivity.this, InfoActivity.class);
+        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        //pending Intent 暫緩???
+        PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 123, it,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+
         builder.setContentTitle("測試");
         builder.setContentText("這是內容");
-        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        builder.setSmallIcon(R.mipmap.ic_launcher_round);
+        //設定點完這個Notifycationon就消失
+        builder.setAutoCancel(true);
+        builder.setContentIntent(pi);
         Notification notify = builder.build();
         nm.notify(1, notify);
 
